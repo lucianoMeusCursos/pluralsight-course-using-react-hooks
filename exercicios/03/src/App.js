08ab040e3601e62b9cb06e41e1642ca840222528
Luciano Barauna <baraunaluciano@gmail.com>
2022-04-01 18:04:04 -0300

    finalizado clip3  - aula 06

--- exercicios/03/src/App.js
+++ exercicios/03/src/App.js
@@ -2,10 +2,25 @@ import React from 'react';
 import Home from './Home.js';
 import Speakers from './Speakers';
 
-const App = ({ pageName }) => {
-  if (pageName === 'Home') return <Home></Home>;
-  if (pageName === 'Speakers') return <Speakers></Speakers>;
+export const ConfigContext = React.createContext();
+
+const pageToShow = (pageName) => {
+  if (pageName === 'Home') return <Home />;
+  if (pageName === 'Speakers') return <Speakers />;
   return <div>Not Found</div>;
+};
+
+const configValue = {
+  showSignMeUp: false,
+  showSpeakerSpeakingDays: false,
+};
+
+const App = ({ pageName }) => {
+  return (
+    <ConfigContext.Provider value={configValue}>
+      <div>{pageToShow(pageName)}</div>
+    </ConfigContext.Provider>
+  );
 };
 
 export default App;
--- exercicios/03/src/Speakers.js
+++ exercicios/03/src/Speakers.js
@@ -1,11 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 import { Header } from './Header';
 import { Menu } from './Menu';
 import SpeakerData from './SpeakerData';
 import SpeakerDetail from './SpeakerDetail';
 
+import { ConfigContext } from './App';
+
 const Speakers = ({}) => {
+  const context = useContext(ConfigContext);
   const [speakingSaturday, setSpeakingSaturday] = useState(true);
   const [speakingSunday, setSpeakingSunday] = useState(true);
 
@@ -74,30 +77,32 @@ const Speakers = ({}) => {
       <Menu />
       <div className="container">
         <div className="btn-toolbar margintopbottom5 chekbox-bigger">
-          <div className="hide">
-            <div className="form-check-inline">
-              <label className="form-check-label">
-                <input
-                  type="checkbox"
-                  className="form-check-input"
-                  onChange={handleChangeSaturday}
-                  checked={speakingSaturday}
-                />
-                Saturday Speakers
-              </label>
+          {context.showSpeakerSpeakingDays === false ? null : (
+            <div className="hide">
+              <div className="form-check-inline">
+                <label className="form-check-label">
+                  <input
+                    type="checkbox"
+                    className="form-check-input"
+                    onChange={handleChangeSaturday}
+                    checked={speakingSaturday}
+                  />
+                  Saturday Speakers
+                </label>
+              </div>
+              <div className="form-check-inline">
+                <label className="form-check-label">
+                  <input
+                    type="checkbox"
+                    className="form-check-input"
+                    onChange={handleChangeSunday}
+                    checked={speakingSunday}
+                  />
+                  Sunday Speakers
+                </label>
+              </div>
             </div>
-            <div className="form-check-inline">
-              <label className="form-check-label">
-                <input
-                  type="checkbox"
-                  className="form-check-input"
-                  onChange={handleChangeSunday}
-                  checked={speakingSunday}
-                />
-                Sunday Speakers
-              </label>
-            </div>
-          </div>
+          )}
         </div>
 
         <div className="row">
