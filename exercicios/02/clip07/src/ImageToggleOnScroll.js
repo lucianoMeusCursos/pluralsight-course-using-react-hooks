69a3b3c90dc7b542c5a2e4464ff4f51f5597cebf
Luciano Barauna <baraunaluciano@gmail.com>
2022-04-01 15:38:07 -0300

    finalizando aula

--- exercicios/02/clip07/src/ImageToggleOnScroll.js
+++ exercicios/02/clip07/src/ImageToggleOnScroll.js
@@ -1,29 +1,45 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from 'react';
 
-const ImageToggleOnScroll = ({ primaryImg, secondaryImg}) => {
-  const imageRef = useRef(null)
-  const [ inView, setInView ] = useState(false)
+const ImageToggleOnScroll = ({ primaryImg, secondaryImg }) => {
+  const imageRef = useRef(null);
+
+  const [isLoading, setIsLoading] = useState(true);
 
   const isInView = () => {
     const rect = imageRef.current.getBoundingClientRect();
-    console.log(rect)
-    return rect.top >= 0 && rect.bottom <= window.innerHeight
-  }
+    return rect.top >= 0 && rect.bottom <= window.innerHeight;
+  };
+
+  const [inView, setInView] = useState(false);
 
   useEffect(() => {
-    window.addEventListener("scroll", scrollHandler);
+    setIsLoading(false);
+    setInView(isInView());
+    window.addEventListener('scroll', scrollHandler);
     return () => {
-      window.removeEventListener("scroll", scrollHandler)
-    }
-  }, [])
+      window.removeEventListener('scroll', scrollHandler);
+    };
+  }, []);
 
   const scrollHandler = () => {
-    setInView(isInView())
-  }
+    setInView(isInView());
+  };
 
   return (
-    <img src={inView ? secondaryImg : primaryImg} ref={imageRef} alt="" />
-  )
-}
+    <img
+      src={
+        isLoading
+          ? 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==' // 1x1gif
+          : inView
+          ? secondaryImg
+          : primaryImg
+      }
+      alt=""
+      ref={imageRef}
+      width="200"
+      height="200"
+    />
+  );
+};
 
-export default ImageToggleOnScroll
+export default ImageToggleOnScroll;
